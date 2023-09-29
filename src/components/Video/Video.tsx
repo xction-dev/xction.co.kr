6f8386f7e8db502c5a34dcfa706739d0e12ea83c
nuagenic <nuagenic@gmail.com>
2023-09-29 21:46:22 +0900

    feat: Add 'watch' page and video components

---
+++ src/components/Video/Video.tsx
@@ -0,0 +1,11 @@
+import "./Video.css";
+
+export default function Video() {
+  return (
+    <>
+      <video src="/assets/ETA.mp4" width="500px" controls loop>
+        Video Not Provided.
+      </video>
+    </>
+  );
+}
