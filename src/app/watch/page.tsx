b091d0ae076df9c5250d9b977bd58aa0a4048e78
nuagenic <nuagenic@gmail.com>
2023-10-03 15:34:20 +0900

    Refact: Rename 'Video' components to 'Watch'

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,4 +1,8 @@
+<<<<<<< refs/remotes/origin/main
 import Player from "@/components/Watch/Player/Player";
+=======
+import Video from "@/components/Watch/Video";
+>>>>>>> Refact: Rename 'Video' components to 'Watch'
 
 export default function Watch() {
   return (
