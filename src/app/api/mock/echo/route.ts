b423c0c1aa09bf0310c0f7cce71af7cdc316fdce
designDefined <designdefined.by@gmail.com>
2023-10-03 15:53:46 +0900

    feat: Add echo api

---
+++ src/app/api/mock/echo/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    return NextResponse.json(body);
+  } catch (e) {
+    // default: unknown error
+    const errorResponse: { status: number; data: unknown } = {
+      status: 500,
+      data: { error_type: 0, error_message: "unknown error" },
+    };
+
+    // handle JSON error
+    if (e instanceof SyntaxError && e.message.includes("JSON")) {
+      errorResponse.status = 402;
+      errorResponse.data = {
+        error_type: 1,
+        error_message: "Body should be a valid JSON object",
+      };
+    }
+
+    // send error response
+    return NextResponse.json(errorResponse.data, {
+      status: errorResponse.status,
+    });
+  }
+}
