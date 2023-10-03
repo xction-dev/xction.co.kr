d58e8b633559d94eeb35a8e0c4c6602b1452108d
nuagenic <nuagenic@gmail.com>
2023-10-03 16:55:16 +0900

    refact: Make separate video contols components

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,8 +1,12 @@
 <<<<<<< refs/remotes/origin/main
+<<<<<<< refs/remotes/origin/main
 import Player from "@/components/Watch/Player/Player";
 =======
 import Video from "@/components/Watch/Video";
 >>>>>>> Refact: Rename 'Video' components to 'Watch'
+=======
+import Player from "@/components/Watch/Player/Player";
+>>>>>>> refact: Make separate video contols components
 
 export default function Watch() {
   return (
