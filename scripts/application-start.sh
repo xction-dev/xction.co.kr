1dfd0b45444e86128d98944ce02ceb350f0f650d
Kim Yoonsik <yunsik29@naver.com>
2023-10-08 04:40:17 +0900

    fix: Modify bash

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -3,7 +3,7 @@ pm2 describe app > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start yarn --name app -- start
+  pm2 start yarn -w -i 0 --name "next" -- start
 else
   pm2 reload app
 fi;
