200185aca2b8dab0e2f1f609bf74feeab113d999
Kim Yoonsik <yunsik29@naver.com>
2023-10-08 04:46:00 +0900

    fix: Modify bash 2

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,11 +3,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 16.14.2
-npm install --global yarn
-npm install pm2 -g
-
-ln -sf $(which npm) /usr/local/bin/npm
-ln -sf $(which node) /usr/local/bin/node
-ln -sf $(which yarn) /usr/local/bin/yarn
-ln -sf $(which pm2) /usr/local/bin/pm2
+nvm install --lts
+nvm use --lts
+npm i -g yarn
+npm i -g pm2
