0d03d91625576acdd73f97f150737aaef278ba11
nuagenic <nuagenic@gmail.com>
2023-10-08 15:30:18 +0900

    feat: Add fetching from mock api

--- src/app/watch/[id]/page.tsx
+++ src/app/watch/[id]/page.tsx
@@ -1,5 +1,6 @@
 <<<<<<< refs/remotes/origin/main
 <<<<<<< refs/remotes/origin/main
+<<<<<<< refs/remotes/origin/main
 import Player from "@/components/Watch/Player/Player";
 =======
 import Video from "@/components/Watch/Video";
@@ -7,11 +8,44 @@ import Video from "@/components/Watch/Video";
 =======
 import Player from "@/components/Watch/Player/Player";
 >>>>>>> refact: Make separate video contols components
+=======
+"use client";
 
-export default function Watch() {
+import Player from "@/components/Watch/Player/Player";
+import { useState, useEffect } from "react";
+
+export default function Watch(props) {
+  const id = props.params.id;
+  const [videoSource, setVideoSource] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const callVideo = async () => {
+    try {
+      const response = await fetch(`/api/mock/project/${id}`);
+
+      if (response.ok) {
+        const data = await response.json();
+        setVideoSource(data.src);
+      } else {
+        const data = await response.json();
+        setErrorMessage(data.error_message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      callVideo();
+    }
+  }, [id]);
+>>>>>>> feat: Add fetching from mock api
+
   return (
     <>
-      <Player src="assets/ETA.mp4" />
+      <Player src={videoSource} />
+      {errorMessage && <p>{errorMessage}</p>}
     </>
   );
 }
