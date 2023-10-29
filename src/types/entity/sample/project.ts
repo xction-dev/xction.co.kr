737db5321113ffe794c55e83bf9344877e46f415
designDefined <designdefined.by@gmail.com>
2023-10-29 18:13:23 +0900

    feat: Add sample user entity for example

---
+++ src/types/entity/sample/project.ts
@@ -0,0 +1,5 @@
+export type ProjectEntity = {
+  id: number;
+  title: string;
+  src: string;
+};
---
+++ src/types/entity/sample/user.ts
@@ -0,0 +1,5 @@
+export type SampleUserEntity = {
+  id: number;
+  email: string;
+  password: string;
+};
