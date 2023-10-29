8ce73f6384ac92d85c272d930876bab5c53ca1f3
designDefined <designdefined.by@gmail.com>
2023-10-29 18:16:21 +0900

    feat: Add SampleUserService DTO

---
+++ src/types/dto/SampleUserService/index.ts
@@ -0,0 +1,12 @@
+/**
+ * SampleUserService라는 유즈케이스에 귀속된 DTO이므로
+ * 같은 이름으로 폴더를 만들어 배치합니다.
+ */
+export type PostSampleLoginDto = {
+  email: string;
+  password: string;
+};
+
+export type SampleUserDto = {
+  name: string;
+};
