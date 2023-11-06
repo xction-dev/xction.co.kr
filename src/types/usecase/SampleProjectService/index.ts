9df788121ed10b08876df3326cd9dddec2d01869
nuagenic <nuagenic@gmail.com>
2023-11-06 21:56:11 +0900

    refact: Delete 'body' in usecase 'SampleProjectService'

--- src/types/usecase/SampleProjectService/index.ts
+++ src/types/usecase/SampleProjectService/index.ts
@@ -6,7 +6,7 @@ export type SampleProjectService<ProjectData> = {
    * 영상 재생이 끝났을 때 호출하는 함수입니다. (영상이 끝나거나, 중간에 나갔을 때)
    * `/api/mock/project/:project_id/finish` 엔드포인트로 POST 요청을 보냅니다.
    */
-  finishWathcingProject: (body: any) => void;
+  finishWathcingProject: () => void;
 } & (
   | {
       status: "fetching";
