42f229e5d95f0a11a2b070db6e4fe3f49affdd58
nuagenic <nuagenic@gmail.com>
2023-11-13 14:17:46 +0900

    feat: Create 'getUser' and 'postUserLogin' function

---
+++ client/src/api/UserAccessService/index.ts
@@ -0,0 +1,22 @@
+/**
+ * UserAccessService의 유즈케이스에서 사용할 fetch 함수를 정의합니다.
+ * 유즈케이스를 실제로 구현한 hook이 너무 커지는 것을 방지하기 위해 분리해두었습니다.
+ */
+
+import {
+  PostUserLoginRequestDto,
+  PostUserLoginResponseDto,
+} from "@core/dto/UserAccessService";
+
+export const getUser = () =>
+  fetch(`/api/mock/user`).then((res) =>
+    res.ok ? res.json() : Promise.reject(res),
+  );
+
+export const postUserLogin = (
+  body: PostUserLoginRequestDto,
+): Promise<PostUserLoginResponseDto> =>
+  fetch(`/api/mock/user/login`, {
+    method: "POST",
+    body: JSON.stringify(body),
+  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
