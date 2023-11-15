073ac2b994042e21ce69485df51477bc3205d1f8
nuagenic <nuagenic@gmail.com>
2023-11-15 16:05:30 +0900

    refact: Erase comments in 'api/UserAccessService'

--- client/src/api/UserAccessService/index.ts
+++ client/src/api/UserAccessService/index.ts
@@ -1,8 +1,3 @@
-/**
- * UserAccessService의 유즈케이스에서 사용할 fetch 함수를 정의합니다.
- * 유즈케이스를 실제로 구현한 hook이 너무 커지는 것을 방지하기 위해 분리해두었습니다.
- */
-
 import {
   PostUserLoginRequestDto,
   PostUserLoginResponseDto,
