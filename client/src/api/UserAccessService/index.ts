1c413324f161d50d1c8a0b4f109a2e3bbe8f4738
nuagenic <nuagenic@gmail.com>
2023-11-15 16:09:11 +0900

    refact: Erase 'async' in 'api/UserAccessServce'

--- client/src/api/UserAccessService/index.ts
+++ client/src/api/UserAccessService/index.ts
@@ -11,17 +11,17 @@ export const postUserLogin = (
   fetch(`/api/user/login`, {
     method: "POST",
     body: JSON.stringify(body),
-  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)));
 
 export const postUserAutoLogin = (): Promise<PostUserLoginResponseDto> =>
   fetch(`/api/user/login/auto`, {
     method: "POST",
-  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)));
 
 export const postUserLogout = (): Promise<void> =>
   fetch(`/api/user/logout`, {
     method: "POST",
-  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)));
 
 export const postUserRegister = (
   body: PostUserRegisterRequestDto,
@@ -29,4 +29,4 @@ export const postUserRegister = (
   fetch(`/api/user/register`, {
     method: "POST",
     body: JSON.stringify(body),
-  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
+  }).then((res) => (res.ok ? res.json() : Promise.reject(res)));
