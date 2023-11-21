4f298d96710d092c61e377e8f16338f8e68b7c1a
designDefined <designdefined.by@gmail.com>
2023-11-21 15:23:38 +0900

    feat: Normalize css & reset default layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,6 @@
+import type { Metadata } from "next";
 import Providers from "@/utils/next-query-resolver/Providers";
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +15,7 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </html>
     </Providers>
   );
