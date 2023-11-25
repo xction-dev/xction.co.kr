92ff7e734fe145d5a502673b7e6d029510ce5e58
nuagenic <nuagenic@gmail.com>
2023-11-26 01:10:15 +0900

    feat: Complete Black Button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,8 +1,9 @@
 /**  @jsxImportSource @emotion/react */
 "use client";
 
-import { css } from "@emotion/react";
+import { css, useTheme } from "@emotion/react";
 import { Button as MuiButton } from "@mui/material";
+// import { Theme as MuiTheme } from "@mui/material/styles";
 import typography from "@/styles/typography";
 
 type ButtonProps = Parameters<typeof MuiButton>[0];
@@ -13,13 +14,20 @@ function black(props: ButtonProps) {
       /** MUI의 기본 Ripple Effect 비활성화 시켰습니다. 활성화할지 논의 필요합니다. */
       disableRipple
       css={css`
-        background-color: black;
+        background-color: #343434;
         color: #f6f6f6;
         border-radius: 34px;
         height: 68px;
         padding: 16px 32px;
         box-shadow: 9px 9px 9px rgba(0, 0, 0, 0.2);
         ${typography.h5}
+        &:hover {
+          background-color: #7c7c7c;
+        }
+        &:active {
+          background-color: #343434;
+          border: 2px solid #f1f1f1;
+        }
       `}
       {...props}
     >
@@ -55,7 +63,59 @@ function white(props: ButtonProps) {
   );
 }
 
+function capsule(props: ButtonProps) {
+  return (
+    <MuiButton
+      disableRipple
+      css={css`
+        background-color: gray;
+        color: white;
+        border-radius: 8px;
+        height: 57px;
+        padding: 16px 24px;
+        &:hover {
+          background-color: lightgray;
+        }
+        &:active {
+          background-color: gray;
+          border: 1px solid white;
+        }
+        ${typography.h6}
+      `}
+      {...props}
+    >
+      {props.children}
+    </MuiButton>
+  );
+}
+
+function text(props: ButtonProps) {
+  return (
+    <MuiButton
+      disableRipple
+      css={css`
+        background-color: none;
+        color: black;
+        border-radius: 8px;
+        height: 57px;
+        padding: 16px 24px;
+        &:hover {
+          background-color: lightgray;
+        }
+        &:active {
+          background-color: gray;
+          border: 1px solid white;
+        }
+        ${typography.h6}
+      `}
+      {...props}
+    >
+      {props.children}
+    </MuiButton>
+  );
+}
+
 // const Button = { black, white, capsule, proceed, text };
-const Button = { black, white };
+const Button = { black, white, capsule, text };
 
 export default Button;
