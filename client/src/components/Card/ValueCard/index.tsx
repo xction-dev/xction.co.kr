4d88e5b3f3718ea4d62e3a058a305597dd2731c2
Baek Chang In <128976154+nuagenic@users.noreply.github.com>
2024-03-07 10:18:09 +0900

    [Feat] scroll test (#33)
    * feat: Add CSS scroll-snap TestPage
    
    * feat/Add card component
    
    * fix/Modify Card css

---
+++ client/src/components/Card/ValueCard/index.tsx
@@ -0,0 +1,63 @@
+/**  @jsxImportSource @emotion/react */
+"use client";
+
+import { css } from "@emotion/react";
+import { Card as MuiCard } from "@mui/material";
+import Heading from "@/components/Heading";
+import styled from "@emotion/styled";
+import typography from "@/styles/typography";
+
+type CardProps = Parameters<typeof MuiCard>[0];
+
+/**
+ * ValueCard는 제목과 본문으로 이루어진 카드 컴포넌트입니다.
+ */
+
+export default function ValueCard({ title, content, ...props }: CardProps) {
+  return (
+    <MuiCard
+      css={css`
+        background: linear-gradient(
+          to right,
+          rgba(255, 255, 255, 0.1),
+          rgba(255, 255, 255, 0)
+        );
+        color: white;
+        border: 1px solid white;
+        border-radius: 8px;
+        width: 784px;
+        height: 442px;
+        padding: 24px 48px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+      `}
+      {...props}
+    >
+      <Heading.H4
+        css={css`
+          flex: 20%;
+          display: flex;
+          margin: 24px;
+          align-items: center;
+        `}
+      >
+        {title}
+      </Heading.H4>
+      <p
+        css={css`
+          flex: 80%;
+          display: flex;
+          margin: 24px;
+          align-items: center;
+          white-space: pre-wrap;
+          text-align: center;
+          ${typography.h6}
+        `}
+      >
+        {content}
+      </p>
+    </MuiCard>
+  );
+}
