2bdb14edf07dd26a031cb7ca990d896557d542f5
nuagenic <nuagenic@gmail.com>
2024-04-04 22:23:37 +0900

    fix: Change fetch url to absolute path

--- client/src/api/LogInService/index.ts
+++ client/src/api/LogInService/index.ts
@@ -1,7 +1,7 @@
 import { LogInRequestDto, LogInResponseDto } from "@core/dto/LogInService";
 
 export const postLogIn = (body: LogInRequestDto): Promise<LogInResponseDto> =>
-  fetch(`/users/signin`, {
+  fetch(`http://localhost:8000/users/signin`, {
     method: "POST",
     body: JSON.stringify(body),
   }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
