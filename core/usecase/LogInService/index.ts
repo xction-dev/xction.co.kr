38004097f22bbea0176f75532f374b73d46849fb
nuagenic <nuagenic@gmail.com>
2024-04-05 17:28:38 +0900

    refact: Fix typo

--- core/usecase/LogInService/index.ts
+++ core/usecase/LogInService/index.ts
@@ -1,7 +1,8 @@
 /**
  * 로그인 서비스 유즈케이스입니다.
- * 로그인을 시도하는 tryLogin 함수를 가지고 있습니다.
+ * 로그인을 시도하는 tryLogin 함수(intent)를 가지고 있습니다.
  */
+
 export type LogInService<LoginRequestBody> = {
   tryLogin: (body: LoginRequestBody) => void;
 };
