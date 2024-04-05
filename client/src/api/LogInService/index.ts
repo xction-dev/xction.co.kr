fa186aed5f4ec53221af481716a0a064eff39605
nuagenic <nuagenic@gmail.com>
2024-04-05 19:11:28 +0900

    feat: Receive token in postLogIn

--- client/src/api/LogInService/index.ts
+++ client/src/api/LogInService/index.ts
@@ -1,10 +1,28 @@
 import { LogInRequestDto, LogInResponseDto } from "@core/dto/LogInService";
 
-export const postLogIn = (body: LogInRequestDto): Promise<LogInResponseDto> =>
-  fetch(`http://localhost:8000/users/signin`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
+export const postLogIn = async (
+  body: LogInRequestDto,
+): Promise<LogInResponseDto> => {
+  try {
+    const res = await fetch(`http://localhost:8000/users/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      const errorMessage = await res.text();
+      throw new Error(errorMessage);
+    }
+
+    const { token } = await res.json();
+    sessionStorage.setItem("token", token);
+
+    return { token };
+  } catch (error) {
+    console.error("An error occurred during sign-up:", error);
+    throw error;
+  }
+};
