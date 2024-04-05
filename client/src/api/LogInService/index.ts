d9114531f69c8bd9c39411baf36e88e1c8daf5a6
nuagenic <nuagenic@gmail.com>
2024-04-05 18:00:41 +0900

    refact: Refact SignIn code

--- client/src/api/LogInService/index.ts
+++ client/src/api/LogInService/index.ts
@@ -3,5 +3,8 @@ import { LogInRequestDto, LogInResponseDto } from "@core/dto/LogInService";
 export const postLogIn = (body: LogInRequestDto): Promise<LogInResponseDto> =>
   fetch(`http://localhost:8000/users/signin`, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(body),
   }).then(async (res) => (res.ok ? res.json() : Promise.reject(res)));
--- client/src/modules/LogIn/index.tsx
+++ client/src/modules/LogIn/index.tsx
@@ -7,31 +7,28 @@ import { LogInRequestDto } from "@core/dto/LogInService";
 export default function LogInModule() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const data: LogInRequestDto = { username: username, password: password };
   const { tryLogin } = useLogInService();
+
+  const handleSubmit = () => {
+    const data: LogInRequestDto = { username: username, password: password };
+    tryLogin(data);
+  };
 
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          tryLogin(data);
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log In</button>
-      </form>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Log In</button>
     </div>
   );
 }
