fa1eca8305979e979e2f22d07f44973b69d42835
nuagenic <nuagenic@gmail.com>
2024-04-09 22:10:26 +0900

    feat: Update Color Variables

--- client/src/styles/color.ts
+++ client/src/styles/color.ts
@@ -13,6 +13,26 @@ const gray600 = "#7c7c7c";
 const gray800 = "#343434";
 const gray900 = "#202020";
 
+/**
+ * Brand Color
+ */
+const darkGreen = "24322C";
+const darkGreenDark = "1E2522";
+const darkGreenLight = "3C795E";
+const bronze = "E7D0BA";
+const bronzeDark = "AA835D";
+const bronzeLight = "FFEEDD";
+const olive = "6D9042";
+const oliveDark = "3D482F";
+const oliveLight = "D0F4A5";
+const purple = "D97CE8";
+const pink = "EE77C6";
+const blue = "6E93DA";
+const navy = "4F5EAE";
+const green = "58B256";
+const mint = "51C09F";
+const orange = "EE8C45";
+
 const color = {
   gray50,
   gray200,
@@ -20,6 +40,22 @@ const color = {
   gray600,
   gray800,
   gray900,
+  darkGreen,
+  darkGreenDark,
+  darkGreenLight,
+  bronze,
+  bronzeDark,
+  bronzeLight,
+  olive,
+  oliveDark,
+  oliveLight,
+  purple,
+  pink,
+  blue,
+  navy,
+  green,
+  mint,
+  orange,
 } as const;
 
 export default color;
