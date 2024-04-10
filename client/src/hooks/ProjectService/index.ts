3135f916e105d535b81ab4b19c6d8a9ae961c286
lenyakim <72087339+lenyakim@users.noreply.github.com>
2024-04-10 20:30:34 +0900

    feat/ProjectService-hooks

--- client/src/hooks/ProjectService/index.ts
+++ client/src/hooks/ProjectService/index.ts
@@ -1,50 +1,35 @@
 import { useMemo } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { PlayProjectService } from "@core/usecase/PlayProjectService";
-import {
-  getSampleProjectById,
-  postSampleProjectById,
-} from "@/api/SampleProjectService";
-import { GetProjectResponseDto } from "@core/dto/ProjectService";
+import { getSampleProjectById, postSampleProjectById } from "@core/usecase/PlayProjectService";
 
-type InjectedUsecase = PlayProjectService;
 
-export const useProjectService = (id: string): InjectedUsecase => {
+export const useProjectService = (id: string) => {
   const queryClient = useQueryClient();
 
-  const { data, error, status } = useQuery({
-    queryKey: ["project", id],
-    queryFn: getSampleProjectById(id),
+  const { data, error, status } = us eQuery(["project", id], () => getSampleProjectById(id), {
     staleTime: 1000 * 60,
     gcTime: 1000 * 60 * 60,
     retry: 0,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: postSampleProjectById(id),
+  const { mutate } = useMutation(() => postSampleProjectById(id), {
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["project", id] });
+      queryClient.invalidateQueries(["project", id]);
     },
   });
 
-  const parsedFetchResult = useMemo(() => {
+  const project = useMemo(() => {
     switch (status) {
       case "pending":
-        return {
-          status: "fetching",
-          project: data ?? null,
-          error: error ?? null,
-        } as const;
+        return { status: "fetching", project: null, error: null };
       case "error":
-        return { status: "fail", project: null, error } as const;
+        return { status: "fail", project: null, error };
       case "success":
-        return { status: "success", project: data, error: null } as const;
+        return { status: "success", project: data, error: null };
     }
   }, [status, data, error]);
 
-  const finishWatchingProject = () => {
-    mutate();
-  };
+  const finishWatchingProject = () => mutate();
 
-  return { ...parsedFetchResult, finishWatchingProject };
+  return { ...project, finishWatchingProject };
 };
