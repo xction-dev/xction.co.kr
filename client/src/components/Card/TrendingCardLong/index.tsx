ff815816ddce1e425cddf1e1f49be10cc3b2f895
nuagenic <nuagenic@gmail.com>
2024-04-10 19:47:42 +0900

    refact: Format date for TrendingCardLong

---
+++ client/src/components/Card/TrendingCardLong/index.tsx
@@ -0,0 +1,51 @@
+/**
+ * 실시간 인기글을 보여주는 카드 컴포넌트입니다.
+ * 추후 고정된 전체 width 값을 반응형으로 구현하며, 태그 작업이 필요합니다.
+ */
+
+import typography from "../../../styles/typography.module.css";
+import styles from "./TrendingCardLong.module.css";
+
+type TrendingPost = {
+  title: string;
+  views: number;
+  date: Date;
+  tag: string;
+};
+
+type TrendingCardProps = {
+  data: TrendingPost[];
+};
+
+export default function TrendingCardLong({ data }: TrendingCardProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h4 className={typography.h4}>실시간 인기글</h4>
+      </div>
+      <div className={styles.postContainer}>
+        {data.map((post, index) => (
+          <div className={styles.postItemContainer} key={index}>
+            <div className={styles.postInfoContainer}>
+              <h5 className={typography.h5}>{index + 1}</h5>
+              <a href="">
+                <h5 className={typography.h5}>{post.title}</h5>
+              </a>
+              <h6 className={typography.h6}>{post.tag}</h6>
+            </div>
+            <div className={styles.postViewsDateContainer}>
+              <h6 className={typography.h6}>{`${
+                post.views
+              }회 | ${post.date.getFullYear()}.${(post.date.getMonth() + 1)
+                .toString()
+                .padStart(2, "0")}.${post.date
+                .getDate()
+                .toString()
+                .padStart(2, "0")}`}</h6>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
