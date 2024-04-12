57a1c708665e1a60240de63e96cbf3b2dde6ded7
nuagenic <nuagenic@gmail.com>
2024-04-12 14:52:58 +0900

    feat: Add community page

---
+++ client/src/app/community/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Header from "../../components/Header";
+import "./Community.css";
+
+export default function Community() {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+}
