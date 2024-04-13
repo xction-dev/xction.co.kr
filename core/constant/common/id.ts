4261183aea499387093411e46305dd0358c4d132
designDefined <designdefined.by@gmail.com>
2024-04-13 19:53:56 +0900

    feat: Add user constant & entity

---
+++ core/constant/common/id.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+/*
+ * Primitives
+ */
+export const INTEGER_ID = z.number().int().nonnegative();
+export type INTEGER_ID = z.infer<typeof INTEGER_ID>;
+
+export const STRING_ID = z.string().min(1);
+export type STRING_ID = z.infer<typeof STRING_ID>;
+
+/*
+ * Entities
+ */
+export const ID = {
+  USER: INTEGER_ID,
+  WORK: INTEGER_ID,
+  ARTICLE: INTEGER_ID,
+  POST: INTEGER_ID,
+};
+
+export type ID = {
+  [key in keyof typeof ID]: z.infer<(typeof ID)[key]>;
+};
