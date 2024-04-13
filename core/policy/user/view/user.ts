c7611738c684a0bca48d17d6c71a3a06355a0025
designDefined <designdefined.by@gmail.com>
2024-04-13 19:59:38 +0900

    feat: Add sample policies

---
+++ core/policy/user/view/user.ts
@@ -0,0 +1,12 @@
+import { ID } from "@core/constant/common/id";
+import { User } from "@core/entity/user";
+import { VP } from "@core/policy/viewPolicyFactory";
+
+/**
+ * @viewPolicy User
+ * @description 특정 유저의 상세 정보를 조회합니다
+ */
+export const VPUser = VP((userId: ID["USER"]) => ({
+  key: [{ user: userId }],
+  model: User,
+}));
--- core/policy/viewPolicy.ts
+++ core/policy/viewPolicy.ts
@@ -1,1 +1,9 @@
-export const viewPolicy = {};
+import { VPMe } from "./user/view/me";
+import { VPUser } from "./user/view/user";
+
+export const viewPolicy = {
+  user: {
+    me: VPMe,
+    user: VPUser,
+  },
+};
