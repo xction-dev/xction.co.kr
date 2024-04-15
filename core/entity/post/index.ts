0c71952f9a1c245d7e7f3b925bd18e2f77f05779
designDefined <designdefined.by@gmail.com>
2024-04-15 12:07:17 +0900

    feat: Connect comment entity

--- core/entity/post/index.ts
+++ core/entity/post/index.ts
@@ -2,6 +2,7 @@ import { ID } from "@core/constant/common/id";
 import { z } from "zod";
 import { Creatable } from "../utility/creatable";
 import { Tag } from "../tag";
+import { PostComment } from "../comment/post";
 
 /**
  * @entity Post
@@ -15,6 +16,7 @@ export const Post = z
     tags: Tag.array(),
     likesCount: z.number().int().nonnegative(),
     viewsCount: z.number().int().nonnegative(),
+    comments: PostComment.array(),
   })
   .extend(Creatable.shape);
 export type Post = z.infer<typeof Post>;
