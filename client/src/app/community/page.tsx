f3c87e968efc953018a65148a635daf413891297
nuagenic <nuagenic@gmail.com>
2024-04-19 12:42:11 +0900

    feat: Import Header and TrendingCard to Community Page

--- client/src/app/community/page.tsx
+++ client/src/app/community/page.tsx
@@ -1,12 +1,52 @@
 "use client";
 
-import Header from "../../components/Header";
+import Header from "@/components/Header";
+import TrendingCardLong from "@/components/Card/TrendingCardLong";
 import "./Community.css";
 
 export default function Community() {
   return (
     <div className="page">
-      <Header />
+      <div className="header">
+        <Header />
+      </div>
+      <div className="body">
+        <TrendingCardLong data={sampleTrendingData} />
+      </div>  
     </div>
   );
 }
+
+
+const sampleTrendingData = [
+  {
+    title: "1위 포스트",
+    views: 5000,
+    tag: "태그",
+    date: new Date("2021-08-01"),
+  },
+  {
+    title: "2위 포스트",
+    views: 4000,
+    tag: "태그",
+    date: new Date("2021-08-01"),
+  },
+  {
+    title: "3위 포스트",
+    views: 3000,
+    tag: "태그",
+    date: new Date("2021-08-01"),
+  },
+  {
+    title: "4위 포스트",
+    views: 2000,
+    tag: "태그",
+    date: new Date("2021-08-01"),
+  },
+  {
+    title: "5위 포스트",
+    views: 1000,
+    tag: "태그",
+    date: new Date("2021-08-01"),
+  },
+];
