3b46e11b988755635d23202090d34e65616dd1da
nuagenic <nuagenic@gmail.com>
2024-04-28 23:22:39 +0900

    feat: Apply @components alias to community page

--- client/src/app/community/page.tsx
+++ client/src/app/community/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import Header from "@/components/Header";
-import TrendingCardLong from "@/components/Card/TrendingCardLong";
-import Input from "@/components/Input";
-import PostListCard from "@/components/Card/PostListCard";
-import Footer from "@/components/Footer";
+import Header from "@components/Header";
+import TrendingCardLong from "@components/Card/TrendingCardLong";
+import PostWriteButton from "@components/Button/PostWriteButton";
+import Input from "@components/Input";
+import PostListCard from "@components/Card/PostListCard";
+import Footer from "@components/Footer";
 import "./Community.css";
 
 export default function Community() {
@@ -18,12 +19,12 @@ export default function Community() {
           <TrendingCardLong data={sampleTrendingData} />
         </div>
         <div className="InputFieldContainer">
-          <Input inputType="write" />
+          <PostWriteButton />
         </div>
         <div className="PostListCardContainer">
           <PostListCard data={samplePostData} />
         </div>
-      </div>  
+      </div>
       <div className="footer">
         <Footer />
       </div>
@@ -35,41 +36,51 @@ export default function Community() {
 const sampleTrendingData = [
   {
     title: "지원 사업 관련 질문 있습니다!",
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
     viewsCount: 5000,
     createdTime: new Date("2021-08-01"),
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
     viewsCount: 5000,
     createdTime: new Date("2021-08-01"),
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
     viewsCount: 5000,
     createdTime: new Date("2021-08-01"),
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
     viewsCount: 5000,
     createdTime: new Date("2021-08-01"),
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
     viewsCount: 5000,
     createdTime: new Date("2021-08-01"),
   },
@@ -79,7 +90,8 @@ const sampleTrendingData = [
 const samplePostData = [
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -88,16 +100,19 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },
-  {
-    name: "월간Best",
-  }],
+    tags: [
+      {
+        name: "정보",
+      },
+      {
+        name: "월간Best",
+      },
+    ],
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -106,13 +121,16 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -121,13 +139,16 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -136,13 +157,16 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -151,13 +175,16 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
   },
   {
     title: "지원 사업 관련 질문 있습니다!",
-    content: "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+    content:
+      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
     postType: "Information",
     likesCount: 20,
     viewsCount: 100,
@@ -166,9 +193,10 @@ const samplePostData = [
     createdUser: {
       name: "날아오르는 고라파덕",
     },
-    tags: [{
-      name: "정보",
-    },],
+    tags: [
+      {
+        name: "정보",
+      },
+    ],
   },
-  
 ];
