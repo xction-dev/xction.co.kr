11a3b734f518ba9c600b636935d70e9a43c88772
designDefined <designdefined.by@gmail.com>
2024-05-09 21:42:03 +0900

    feat: Delete deprecated

--- core/dto/sample/SampleProjectService/index.ts
+++
@@ -1,7 +0,0 @@
-import { SampleProjectEntity } from "@core/entity/sample/project";
-
-export type GetSampleProjectResponseDto = {
-  id: SampleProjectEntity["id"];
-  title: SampleProjectEntity["title"];
-  src: SampleProjectEntity["src"];
-};
