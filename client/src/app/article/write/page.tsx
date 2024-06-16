db25527d78c67d5e4796db73c0a85557de92972b
designDefined <designdefined.by@gmail.com>
2024-06-16 22:16:50 +0900

    feat: Article write

---
+++ client/src/app/article/write/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import "./ArticleWrite.css";
+import Header from "@components/Header";
+
+import Footer from "@components/Footer";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+
+export default function ArticleWrite() {
+  const [data, setData] = useState("");
+  const router = useRouter();
+
+  const onSubmit = useCallback(() => {
+    fetch("http://localhost:8080/articles", {
+      method: "POST",
+      body: JSON.stringify({ content: data }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        z.object({
+          insertedId: z.number(),
+        }).parse,
+      )
+      .then(({ insertedId }) => router.push(`/article/${insertedId}`));
+  }, [data, router]);
+
+  return (
+    <div className="page">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="body">
+        <input value={data} onChange={(e) => setData(e.target.value)} />
+        <button onClick={onSubmit}>전송</button>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
