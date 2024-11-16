f7a78003626fb392241b06f3e61d19111978565e
nuagenic <nuagenic@gmail.com>
2024-11-16 14:07:08 +0900

    feat: Add tailwind config

---
+++ client/tailwind.config.ts
@@ -0,0 +1,65 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        text: {
+          primary: "var(--primitives_color_gray-900)",
+          secondary: {
+            DEFAULT: "var(--primitives_color_gray-600)",
+            white: "var(--primitives_color_gray-200)",
+          },
+          brand: "var(--primitives_color_gray-900)",
+          invert: "var(--primitives_color_gray-50)",
+        },
+        surface: {
+          primary: "var(--primitives_color_gray-50)",
+          secondary: {
+            DEFAULT: "var(--primitives_color_gray-800)",
+            white: "var(--primitives_color_gray-200)",
+          },
+          third: "var(--primitives_color_gray-600)",
+          invert: "var(--primitives_color_gray-900)",
+        },
+        border: {
+          primary: "var(--primitives_color_gray-50)",
+        },
+      },
+      spacing: {
+        0.5: "var(--primitives_spacing-05)",
+        1: "var(--primitives_spacing-1)",
+        2: "var(--primitives_spacing-2)",
+        3: "var(--primitives_spacing-3)",
+        4: "var(--primitives_spacing-4)",
+        5: "var(--primitives_spacing-5)",
+        6: "var(--primitives_spacing-6)",
+        7: "var(--primitives_spacing-7)",
+        8: "var(--primitives_spacing-8)",
+        9: "var(--primitives_spacing-9)",
+        10: "var(--primitives_spacing-10)",
+        11: "var(--primitives_spacing-11)",
+        12: "var(--primitives_spacing-12)",
+        13: "var(--primitives_spacing-13)",
+        xs: "var(--primitives_spacing-05)",
+        sm: "var(--primitives_spacing-1)",
+        smmd: "var(--primitives_spacing-2)",
+        md: "var(--primitives_spacing-3)",
+        lg: "var(f--primitives_spacing-4)",
+        xl: "var(--primitives_spacing-6)",
+        xxl: "var(--primitives_spacing-13)",
+      },
+      borderRadius: {
+        minimal: "var(--primitives_radius-sm)",
+        rounded: "var(--primitives_radius-md)",
+        full: "var(--primitives_radius-3xl)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
