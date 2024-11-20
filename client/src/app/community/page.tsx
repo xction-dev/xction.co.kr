038d4ef4649b3ccbd54a35f73620f71ded3988ac
designDefined <designdefined.by@gmail.com>
2024-11-20 14:01:16 +0900

    fix: Comment out community mock data

--- client/src/app/community/page.tsx
+++ client/src/app/community/page.tsx
@@ -8,6 +8,7 @@ import Footer from "@components/Footer";
 import "./Community.css";
 import { useEffect } from "react";
 import { PostApi } from "@core/api/post";
+import { Post } from "@core/entity/post";
 
 export default function Community() {
   useEffect(() => {
@@ -37,170 +38,181 @@ export default function Community() {
 }
 
 // 서버 데이터로 대체 예정입니다.
-const sampleTrendingData = [
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-    viewsCount: 5000,
-    createdTime: new Date("2021-08-01"),
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-    viewsCount: 5000,
-    createdTime: new Date("2021-08-01"),
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-    viewsCount: 5000,
-    createdTime: new Date("2021-08-01"),
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-    viewsCount: 5000,
-    createdTime: new Date("2021-08-01"),
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-    viewsCount: 5000,
-    createdTime: new Date("2021-08-01"),
-  },
+const sampleTrendingData: Post[] = [
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  //   viewsCount: 5000,
+  //   createdTime: new Date("2021-08-01"),
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  //   viewsCount: 5000,
+  //   createdTime: new Date("2021-08-01"),
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  //   viewsCount: 5000,
+  //   createdTime: new Date("2021-08-01"),
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  //   viewsCount: 5000,
+  //   createdTime: new Date("2021-08-01"),
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  //   viewsCount: 5000,
+  //   createdTime: new Date("2021-08-01"),
+  // },
 ];
 
 // 서버 데이터로 대체 예정입니다.
-const samplePostData = [
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-      {
-        name: "월간Best",
-      },
-    ],
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-  },
-  {
-    title: "지원 사업 관련 질문 있습니다!",
-    content:
-      "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
-    postType: "Information",
-    likesCount: 20,
-    viewsCount: 100,
-    commentsCount: 5,
-    createdTime: new Date("2021-08-01"),
-    createdUser: {
-      name: "날아오르는 고라파덕",
-    },
-    tags: [
-      {
-        name: "정보",
-      },
-    ],
-  },
+const samplePostData: Post[] = [
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "월간Best",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 1,
+  //   title: "지원 사업 관련 질문 있습니다!",
+  //   content:
+  //     "OO 지원 사업 이번에 지원하시는 분 있나요? 외국인은 국제법과 조약이 정하는 바에 의하여 그 지위가 보장된다. 국가의 세입·세출의 결산, 국가 및 법률이 정한...",
+  //   postType: "INFORMATION",
+  //   likesCount: 20,
+  //   viewsCount: 100,
+  //   commentsCount: 5,
+  //   createdTime: new Date("2021-08-01"),
+  //   createdUser: { id: 1, thumbnailImage: null, name: "날아오르는 고라파덕" },
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "정보",
+  //     },
+  //   ],
+  // },
 ];
