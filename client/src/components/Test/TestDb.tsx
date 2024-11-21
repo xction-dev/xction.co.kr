09aa06881aea9eadcbd4fa38b94ea28312882c97
designDefined <designdefined.by@gmail.com>
2024-11-22 00:57:18 +0900

    feat: Add test component in clinet

---
+++ client/src/components/Test/TestDb.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+
+export const TestDb = () => {
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_DB_HOST ?? "no_env")
+      .then((res) => {
+        console.log("simple success:");
+        res.text().then(console.log);
+      })
+      .catch((err) => {
+        console.error("simple error: ");
+        console.log(err);
+      });
+
+    fetch((process.env.NEXT_PUBLIC_DB_HOST ?? "no_env") + "/db")
+      .then((res) => {
+        console.log("db success:");
+        res.text().then(console.log);
+      })
+      .catch((err) => {
+        console.error("db error: ");
+        console.log(err);
+      });
+  }, []);
+
+  return <div>Test Server & DB</div>;
+};
