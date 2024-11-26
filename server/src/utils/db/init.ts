48aa199f0665db259f60e097348853d109a49442
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 21:05:31 +0900

    fix: Expand connect timeout

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -2,9 +2,10 @@ import { createConnection } from "mysql2/promise";
 
 export const connection = () =>
   createConnection({
-    host: "172.19.0.3",
+    host: "xctiondb",
     port: 3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
+    connectTimeout: 10000,
   });
