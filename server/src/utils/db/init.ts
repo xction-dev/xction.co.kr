2810444582650b8d3c5e8c239fd15c1b95834172
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 23:22:02 +0900

    fix: hard coding db name

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -2,9 +2,9 @@ import { createConnection } from "mysql2/promise";
 
 export const connection = () =>
   createConnection({
-    host: "xctiondb",
+    host: "172.19.0.4",
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
+    database: "xctiondb",
   });
