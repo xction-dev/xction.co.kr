73d0c51be94d35fb5ddf973e0474b125f7613085
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 20:12:23 +0900

    fix: hard coding host name 2

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -2,7 +2,7 @@ import { createConnection } from "mysql2/promise";
 
 export const connection = () =>
   createConnection({
-    host: xctiondb,
+    host: xctiondb:3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
