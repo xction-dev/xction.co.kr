b355d931fb3d03712fa117462bfa41a941b0f664
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 20:44:06 +0900

    fix: input port

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -3,6 +3,7 @@ import { createConnection } from "mysql2/promise";
 export const connection = () =>
   createConnection({
     host: "172.19.0.3",
+    port: 3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
