ebab62307978272ae44fafbe716832f09d45f7a7
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 19:55:18 +0900

    fix: input host name

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -2,7 +2,7 @@ import { createConnection } from "mysql2/promise";
 
 export const connection = () =>
   createConnection({
-    host: process.env.DB_HOST,
+    host: xctiondb
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
