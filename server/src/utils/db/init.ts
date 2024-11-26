e582fff4fadd82a02797fd5087718640874187d9
Kim Yoonsik <yunsik29@naver.com>
2024-11-26 22:37:05 +0900

    fix: Revert host name

--- server/src/utils/db/init.ts
+++ server/src/utils/db/init.ts
@@ -2,8 +2,8 @@ import { createConnection } from "mysql2/promise";
 
 export const connection = () =>
   createConnection({
-    host: "xctiondb",
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_NAME,
